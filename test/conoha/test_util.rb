a294160ebd84a3f78ddd66993089757a1e1f7ad7
ka <ka.kaosf@gmail.com>
2015-09-07 23:08:30 +0900

    Add tests
    ref.
      http://docs.ruby-lang.org/ja/2.2.0/class/Rake=3a=3aTestTask.html

---
+++ test/conoha/test_util.rb
@@ -0,0 +1,12 @@
+require 'test/unit'
+require 'conoha/util'
+
+class TestConohaUtil < Test::Unit::TestCase
+  data(
+    "IPv4 first" => ["111.111.111.111", ["111.111.111.111", "1111:1111:1111:1111:1111:1111:1111:1111"]],
+    "IPv4 last"  => ["111.111.111.111", ["1111:1111:1111:1111:1111:1111:1111:1111", "111.111.111.111"]],
+  )
+  test ".ipv4" do |data|
+    assert_equal data[0], ipv4(data[1])
+  end
+end
