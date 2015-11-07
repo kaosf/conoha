2eb49128e8b4a5ceeaa753e1f2714786281c56df
ka <ka.kaosf@gmail.com>
2015-11-07 14:39:33 +0900

    Implement Conoha::Config

---
+++ lib/conoha/config.rb
@@ -0,0 +1,26 @@
+require 'json'
+
+class Conoha
+  class Config
+    attr_reader :username, :password, :tenant_id, :public_key, :authtoken
+
+    def load_from_yaml!(yaml_str)
+      loaded = YAML.load yaml_str
+      raise Exception.new unless loaded.is_a? Hash
+    end
+
+    def load_from_json!(json_str)
+      parsed = JSON.parse json_str
+      raise Exception.new unless parsed.is_a? Hash
+      @username = parsed["username"]
+      @password = parsed["password"]
+      @tenant_id = parsed["tenant_id"]
+      @public_key = parsed["public_key"]
+      @authtoken = parsed["authtoken"]
+    end
+
+    def authtoken=(authtoken)
+      @authtoken = authtoken
+    end
+  end
+end
---
+++ test/conoha/test_config.rb
@@ -0,0 +1,66 @@
+require 'test_helper'
+require 'conoha/config'
+
+class TestConohaConfig < Test::Unit::TestCase
+  setup do
+    @config = Conoha::Config.new
+  end
+
+  test '#load_from_yaml!' do
+    #pending
+  end
+
+  sub_test_case '#load_from_json!' do
+    sub_test_case 'raise error when invalid json' do
+      test 'empty string' do
+        assert_raise(JSON::ParserError) { @config.load_from_json! '' }
+      end
+
+      test '[]' do
+        assert_raise(Exception) { @config.load_from_json! '[]' }
+      end
+    end
+
+    test 'no authtoken' do
+      json = <<-EOS
+{
+  "username": "a",
+  "password": "b",
+  "tenant_id": "c",
+  "public_key": "d"
+}
+EOS
+      @config.load_from_json! json
+      assert { @config.username   == "a" }
+      assert { @config.password   == "b" }
+      assert { @config.tenant_id  == "c" }
+      assert { @config.public_key == "d" }
+      assert { @config.authtoken.nil? }
+    end
+
+
+    test 'with authtoken' do
+      json = <<-EOS
+{
+  "username": "a",
+  "password": "b",
+  "tenant_id": "c",
+  "public_key": "d",
+  "authtoken": "e"
+}
+EOS
+      @config.load_from_json! json
+      assert { @config.username   == "a" }
+      assert { @config.password   == "b" }
+      assert { @config.tenant_id  == "c" }
+      assert { @config.public_key == "d" }
+      assert { @config.authtoken  == "e" }
+    end
+  end
+
+  test '#authtoken=' do
+    assert { @config.authtoken.nil? }
+    @config.authtoken = 'a'
+    assert { @config.authtoken == 'a' }
+  end
+end
