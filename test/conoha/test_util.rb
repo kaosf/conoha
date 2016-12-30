1d56c6e1dd11ca430ac784807170af225c26ae02
ka <ka.kaosf@gmail.com>
2016-12-31 06:09:35 +0900

    Implement the HTTPS request utilities tests

--- test/conoha/test_util.rb
+++ test/conoha/test_util.rb
@@ -2,6 +2,29 @@ require 'test_helper'
 require 'conoha/util'
 
 class TestConohaUtil < Test::Unit::TestCase
+  sub_test_case 'HTTPS request utilities' do
+    setup do
+      @uri = 'https://example.com/a/b/c'
+      @payload = {}
+      @auth_token = '0123456789abcdef'
+    end
+
+    test "https_get" do
+      any_instance_of(Net::HTTP) { |https| stub(https).request }
+      assert_nothing_raised { https_get @uri, @auth_token }
+    end
+
+    test "https_post" do
+      any_instance_of(Net::HTTP) { |https| stub(https).request }
+      assert_nothing_raised { https_post @uri, @payload, @auth_token }
+    end
+
+    test "https_delete" do
+      any_instance_of(Net::HTTP) { |https| stub(https).request }
+      assert_nothing_raised { https_delete @uri, @auth_token }
+    end
+  end
+
   data(
     "IPv4 first" => ["111.111.111.111", ["111.111.111.111", "1111:1111:1111:1111:1111:1111:1111:1111"]],
     "IPv4 last"  => ["111.111.111.111", ["1111:1111:1111:1111:1111:1111:1111:1111", "111.111.111.111"]],
