4a46ff14d730e117c23c7a95f400686ff18ca658
ka <ka.kaosf@gmail.com>
2017-02-16 23:36:37 +0900

    Implement Conoha::Config::Account class

--- lib/conoha/config.rb
+++ lib/conoha/config.rb
@@ -2,9 +2,29 @@ require 'json'
 
 class Conoha
   class Config
-    attr_reader :username, :password, :tenant_id, :public_key, :authtoken
+    attr_reader :username, :password, :tenant_id, :public_key, :authtoken, :accounts
 
     class Error < StandardError; end
+
+    class Account
+      attr_reader :username, :password, :tenant_id, :public_key
+
+      def initialize(h)
+        @username   = h['username']
+        @password   = h['password']
+        @tenant_id  = h['tenant_id']
+        @public_key = h['public_key']
+      end
+
+      def to_hash
+        {
+          'username'   => @username,
+          'password'   => @password,
+          'tenant_id'  => @tenant_id,
+          'public_key' => @public_key,
+        }
+      end
+    end
 
     def load_from_yaml!(yaml_str)
       loaded = YAML.load yaml_str
@@ -19,6 +39,7 @@ class Conoha
       @tenant_id = parsed["tenant_id"]
       @public_key = parsed["public_key"]
       @authtoken = parsed["authtoken"]
+      @accounts = parsed["accounts"]&.map { |e| Account.new e }
     end
 
     def authtoken=(authtoken)
